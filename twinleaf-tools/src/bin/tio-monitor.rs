ca5817fd8f89dadfb60fa0b1023b9f23b69f73ef
krzywonos <krzywonos@twinleaf.com>
2024-12-10 07:59:55 -0500

    Fixed bug on clearing the screen

--- twinleaf-tools/src/bin/tio-monitor.rs
+++ twinleaf-tools/src/bin/tio-monitor.rs
@@ -1,12 +1,22 @@
-use tio::proto::DeviceRoute;
-use tio::proxy;
-use tio::util;
-use twinleaf::data::{ColumnData, Device};
-use twinleaf::tio;
+use tio::{proto::DeviceRoute, proxy, util};
+use twinleaf::{
+    data::{ColumnData, Device},
+    tio,
+};
 
 use getopts::Options;
-use pancurses::*;
-use std::env;
+use std::{env, io::stdout, time::Duration};
+
+use futures::{future::FutureExt, select, StreamExt};
+use futures_timer::Delay;
+
+use crossterm::ExecutableCommand;
+use crossterm::{
+    cursor::*,
+    event::{Event, EventStream, KeyCode},
+    style::*,
+    terminal::*,
+};
 
 fn tio_opts() -> Options {
     let mut opts = Options::new();
@@ -27,7 +37,7 @@ fn tio_opts() -> Options {
 
 fn tio_parseopts(opts: Options, args: &[String]) -> (getopts::Matches, String, DeviceRoute) {
     let matches = match opts.parse(args) {
-        Ok(m) => m,
+        std::result::Result::Ok(m) => m,
         Err(f) => {
             panic!("{}", f.to_string())
         }
@@ -45,54 +55,100 @@ fn tio_parseopts(opts: Options, args: &[String]) -> (getopts::Matches, String, D
     (matches, root, route)
 }
 
-fn run_monitor(args: &[String]) {
+async fn run_monitor() {
+    let mut reader = EventStream::new();
+
+    let args: Vec<String> = env::args().collect();
+
+    //optional yaml file input
+    let default_path = "default.yaml".to_string();
+    let _path = args.get(2).unwrap_or(&default_path);
+
     let opts = tio_opts();
-    let (_matches, root, route) = tio_parseopts(opts, args);
+    let (_matches, root, route) = tio_parseopts(opts, &args);
 
     let proxy = proxy::Interface::new(&root);
     let device = proxy.device_full(route).unwrap();
     let mut device = Device::new(device);
 
-    //initialize terminal window
-    let window = initscr();
-    window.refresh();
-    noecho();
-    let mut y_position = 3;
+    let mut stdout = stdout();
 
-    loop {
-        let sample = device.next();
-
+    'drawing: loop {
+        let mut delay = Delay::new(Duration::from_nanos(1)).fuse();
+        let mut event = reader.next().fuse();
+        
+        let sample = device.next(); //BREAKING HERE
+        
+        //write in title
         let name = format!(
             "Device Name: {}  Serial: {}   Session ID: {}",
             sample.device.name, sample.device.serial_number, sample.device.session_id
         );
-        window.mvprintw(1, 0, &name);
+        _ = stdout.execute(MoveToRow(0));
+        println!("\r\n{}", name);
+        _ = stdout.execute(MoveToNextLine(1));
+        
 
-        for col in &sample.columns {
-            let string = format!(
-                " {}: {}",
-                col.desc.name,
-                match col.value {
-                    ColumnData::Int(x) => format!("{}", x),
-                    ColumnData::UInt(x) => format!("{:.3}", x),
-                    ColumnData::Float(x) => format!("{:.3}", x),
-                    ColumnData::Unknown => "?".to_string(),
+        select! {
+            _= delay => {
+                for col in &sample.columns{
+                    let string = format!(
+                        " {}: {}",
+                        col.desc.name,
+                        match col.value {
+                            ColumnData::Int(x) => format!("{}", x),
+                            ColumnData::UInt(x) => format!("{:.3}", x),
+                            ColumnData::Float(x) => format!("{:.3}", x),
+                            ColumnData::Unknown => "?".to_string(),
+                        }
+                    );
+
+                    if sample.stream.stream_id == 2 && col.desc.name == sample.columns[0].desc.name.clone() {
+                        _ = stdout.execute(MoveToNextLine(1));
+                    }
+                    _ = stdout.execute(Clear(ClearType::CurrentLine));
+                    println!("{}\r", string);
+                    
+
+
                 }
-            );
-
-            if sample.stream.stream_id == 0x02 {
-                y_position += 1;
+               // _ = stdout.execute(Clear(ClearType::UntilNewLine));
+            },
+            some_event = event => {
+                match some_event {
+                    Some(Ok(event)) => {
+                        if event == Event::Key(KeyCode::Char('q').into()) {
+                            break 'drawing;
+                        } else if event == Event::Key(KeyCode::Esc.into()) {
+                            break 'drawing;
+                        }
+                    }
+                    Some(Err(e)) => println!("Error{}\r", e),
+                    None => continue,
+                }
             }
-
-            window.mvprintw(y_position, 0, &string);
-            window.refresh();
         }
-        y_position = 3;
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn main() -> std::io::Result<()> {
+    let mut stdout = stdout();
 
-    run_monitor(&args);
+    //setup terminal
+    enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(SetBackgroundColor(Color::Black))?;
+    stdout.execute(SetForegroundColor(Color::White))?;
+    stdout.execute(Clear(ClearType::All))?;
+    stdout.execute(SavePosition)?;
+    stdout.execute(Hide)?;
+
+    async_std::task::block_on(run_monitor());
+
+    //clean up terminal on end
+    stdout.execute(LeaveAlternateScreen)?;
+    stdout.execute(Show)?;
+    disable_raw_mode()?;
+
+    Ok(())
 }
