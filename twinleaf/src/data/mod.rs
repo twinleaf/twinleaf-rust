a892fe0377a23aff177131a09602a89c96451a25
Steven Nguyen <nguyen@twinleaf.com>
2025-11-04 15:40:01 -0500

    chore: Remove deprecation warning

--- twinleaf/src/data/mod.rs
+++ twinleaf/src/data/mod.rs
@@ -1,7 +1,5 @@
 mod parser;
 mod sample;
 
-#[deprecated(note = "please use `twinleaf::device::Device` instead")]
-pub use crate::device::Device;
 pub use parser::{DeviceDataParser, DeviceFullMetadata};
 pub use sample::{Column, ColumnData, Sample};
