518b872534262377a5b5f13f2c8147b2371307ea
Steven Nguyen <nguyen@twinleaf.com>
2025-11-25 11:45:25 -0500

    chore: Simplify data module exports

--- twinleaf/src/data/mod.rs
+++ twinleaf/src/data/mod.rs
@@ -2,12 +2,12 @@ mod parser;
 mod sample;
 mod filter;
 mod buffer;
+mod util;
 
 #[cfg(feature = "hdf5")]
 pub mod export;
 
-pub use filter::{ColumnFilter};
+pub use filter::ColumnFilter;
 pub use parser::{DeviceDataParser, DeviceFullMetadata};
 pub use sample::{Column, ColumnData, Sample};
-pub use buffer::{Buffer, AlignedWindow, ColumnBatch, BufferEvent,ReadError};
-pub mod util;
+pub use buffer::{Buffer, BufferEvent, AlignedWindow, ColumnBatch, ReadError};
