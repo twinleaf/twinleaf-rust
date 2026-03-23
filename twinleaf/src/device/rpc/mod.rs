0e669e652eeab7d785869dfe88196335fb996d73
Tom Kornack <kornack@twinleaf.com>
2026-03-23 19:16:58 -0400

    chore: Remove unused RpcListError to suppress a warning

--- twinleaf/src/device/rpc/mod.rs
+++ twinleaf/src/device/rpc/mod.rs
@@ -2,6 +2,6 @@ mod client;
 mod registry;
 mod value;
 
-pub use client::{RpcClient, RpcList, RpcListError};
+pub use client::{RpcClient, RpcList};
 pub use registry::{RpcDescriptor, RpcRegistry};
 pub use value::{DecodeError, EncodeError, RpcValue, RpcValueType};
