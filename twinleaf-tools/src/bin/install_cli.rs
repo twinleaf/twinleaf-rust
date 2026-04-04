6bdf726df6030a9067dbd28adfeed5a7c8b3b9d9
Ilysia Krzywonos <ilysia.krzywonos@gmail.com>
2026-04-04 13:51:30 -0400

    Fix: Working bash and zsh tio tab completion installation

--- twinleaf-tools/src/bin/install_cli.rs
+++ twinleaf-tools/src/bin/install_cli.rs
@@ -4,7 +4,7 @@ use std::{
     fs::{create_dir_all, OpenOptions}, 
     path::PathBuf
 };
-use clap::CommandFactory;
+use clap::{CommandFactory, ValueEnum};
 use clap_complete::{generate_to, Shell};
 use twinleaf_tools::TioCli;
 
@@ -15,7 +15,9 @@ fn get_shell_path(shell:Shell) -> (PathBuf, Option<PathBuf>){
     let config = match shell {
         Shell::Bash => {
             dir.push(".local/share/bash-completion/completions/");
-            None
+            let mut rc = PathBuf::from(&home);
+            rc.push(".bashrc");
+            Some(rc)
         }
         Shell::Zsh => {
             dir.push(".zsh/completions/");
@@ -28,9 +30,8 @@ fn get_shell_path(shell:Shell) -> (PathBuf, Option<PathBuf>){
             None
         }
         Shell::PowerShell => {
-            let mut dir = PathBuf::from(&home);
             dir.push("Documents/PowerShell/Modules/");
-            Some(dir)
+            None
         }
         _ => None,
     };
@@ -38,38 +39,45 @@ fn get_shell_path(shell:Shell) -> (PathBuf, Option<PathBuf>){
 
 }
 
-fn write_zsh_config(completion_dir: &PathBuf, rc_path: &PathBuf) -> Result<()> {
+fn write_config(completion_dir: &PathBuf, rc_path: &PathBuf, write_items: Vec<String>) -> Result<()> {
     let mut file = OpenOptions::new()
         .read(true)
         .append(true)
         .create(true)
         .open(&rc_path)?;
     
-    let dir_to_string = completion_dir.to_string_lossy();
     let reader = BufReader::new(&file);
     let exists = reader
         .lines()
         .filter_map(Result::ok)
-        .any(|line| line.contains(&*dir_to_string));
+        .any(|line| line == &*completion_dir.to_string_lossy());
 
     if !exists {
-        writeln!(&mut file, "autoload -Uz compinit && compinit")?;
-        writeln!(&mut file,"fpath=({:?} $fpath)", completion_dir)?;
+        for item in write_items {
+            writeln!(&mut file, "{}", item)?;
+        }
     } 
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let outdir = Shell::from_env().unwrap_or(Shell::Bash);
-    let (dir, config_path) = get_shell_path(outdir);
-    create_dir_all(&dir)?; //check if completion dir path exists else create it
+    for &shell in Shell::value_variants(){ 
+        let (dir, config_path) = get_shell_path(shell);
+        create_dir_all(&dir)?; //check if completion dir path exists else create it
 
-    let mut tio_cmd = TioCli::command();
-    generate_to(outdir, &mut tio_cmd, "tio", &dir)?; //clap_complete tab complete script generation
+        let mut tio_cmd = TioCli::command();
+        if dir != env::var("HOME").unwrap() {
+            generate_to(shell, &mut tio_cmd, "tio", &dir)?; //clap_complete tab complete script generation
+        }
 
-    if let Some(rc) = config_path {
-        if let Shell::Zsh = outdir {
-            write_zsh_config(&dir, &rc)?;
+        if let Some(rc) = config_path {
+            if let Shell::Zsh = shell {
+                let config = vec![format!("fpath=({} $fpath)", dir.display()), format!("autoload -Uz compinit && compinit")];
+                write_config(&dir, &rc, config)?;
+            } else if let Shell::Bash = shell {
+                let config = vec![format!("source {}tio.bash", dir.display())];
+                write_config(&dir, &rc, config)?;
+            }
         }
     }
     println!("Tab completions successfully installed");
