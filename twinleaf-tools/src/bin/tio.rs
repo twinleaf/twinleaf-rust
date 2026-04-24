3769af9fe8640440fa23bf41dd0ea231246a8ccd
Steven Nguyen <nguyen@twinleaf.com>
2026-04-24 00:48:27 -0400

    refactor: simplify tio main to eyre::Result

--- twinleaf-tools/src/bin/tio.rs
+++ twinleaf-tools/src/bin/tio.rs
@@ -1,5 +1,4 @@
 use clap::{CommandFactory, Parser};
-use std::process::ExitCode;
 use twinleaf_tools::tools::{
     health::run_health,
     monitor::run_monitor,
@@ -14,25 +13,22 @@ use twinleaf_tools::{
     Commands, LogSubcommands, MetaSubcommands, ProxySubcommands, RPCSubcommands, TioCli,
 };
 
-fn main() -> ExitCode {
-    color_eyre::install().expect("color-eyre panic handler should install once");
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
     let cli = TioCli::parse();
 
-    //TODO: Work on exit code logic
-    let result = match cli.command {
+    match cli.command {
         Commands::Proxy(mut proxy_cli) => match proxy_cli.subcommands.take() {
-            Some(ProxySubcommands::Nmea { tio, tcp_port }) => {
-                run_nmea_proxy(tio, tcp_port).map_err(|e| eprintln!("{:?}", e))
-            }
-            None => run_proxy(proxy_cli).map_err(|e| eprintln!("{:?}", e)),
+            Some(ProxySubcommands::Nmea { tio, tcp_port }) => run_nmea_proxy(tio, tcp_port),
+            None => run_proxy(proxy_cli),
         },
         Commands::Monitor {
             tio,
             all,
             fps,
             colors,
-        } => run_monitor(tio, all, fps, colors).map_err(|e| eprintln!("{:?}", e)),
-        Commands::Health(health_cli) => run_health(health_cli).map_err(|e| eprintln!("{:?}", e)),
+        } => run_monitor(tio, all, fps, colors),
+        Commands::Health(health_cli) => run_health(health_cli),
         Commands::Rpc {
             tio,
             subcommands,
@@ -41,31 +37,28 @@ fn main() -> ExitCode {
             req_type,
             rep_type,
             debug,
-        } => {
-            let result: eyre::Result<()> = match subcommands {
-                Some(RPCSubcommands::List { tio }) => list_rpcs(&tio),
-                Some(RPCSubcommands::Dump {
-                    tio,
-                    rpc_name,
-                    capture,
-                }) => rpc_dump(&tio, rpc_name, capture),
-                None => rpc(
-                    &tio,
-                    rpc_name.unwrap_or("".to_string()),
-                    rpc_arg,
-                    req_type,
-                    rep_type,
-                    debug,
-                ),
-            };
-            result.map_err(|e| eprintln!("{:?}", e))
-        }
+        } => match subcommands {
+            Some(RPCSubcommands::List { tio }) => list_rpcs(&tio),
+            Some(RPCSubcommands::Dump {
+                tio,
+                rpc_name,
+                capture,
+            }) => rpc_dump(&tio, rpc_name, capture),
+            None => rpc(
+                &tio,
+                rpc_name.unwrap_or("".to_string()),
+                rpc_arg,
+                req_type,
+                rep_type,
+                debug,
+            ),
+        },
         Commands::Dump {
             tio,
             data,
             meta,
             depth,
-        } => dump(&tio, data, meta, depth).map_err(|e| eprintln!("{:?}", e)),
+        } => dump(&tio, data, meta, depth),
         Commands::Log {
             tio,
             subcommands,
@@ -73,68 +66,58 @@ fn main() -> ExitCode {
             unbuffered,
             raw,
             depth,
-        } => {
-            let result: eyre::Result<()> = match subcommands {
-                Some(LogSubcommands::Meta {
-                    tio,
-                    subcommands,
-                    file,
-                }) => match subcommands {
-                    Some(MetaSubcommands::Reroute {
-                        input,
-                        route,
-                        output,
-                    }) => meta_reroute(input, route, output),
-                    None => log_metadata(&tio, file),
-                },
-                Some(LogSubcommands::Dump {
-                    files,
-                    data,
-                    meta,
-                    sensor,
-                    depth,
-                }) => log_dump(files, data, meta, sensor, depth),
-                Some(LogSubcommands::Csv {
-                    args,
-                    sensor,
+        } => match subcommands {
+            Some(LogSubcommands::Meta {
+                tio,
+                subcommands,
+                file,
+            }) => match subcommands {
+                Some(MetaSubcommands::Reroute {
+                    input,
+                    route,
                     output,
-                }) => log_csv(args, sensor, output),
-                Some(LogSubcommands::Hdf {
-                    files,
-                    output,
-                    filter,
-                    compress,
-                    debug,
-                    split_level,
-                    split_policy,
-                }) => log_hdf(
-                    files,
-                    output,
-                    filter,
-                    compress,
-                    debug,
-                    split_level,
-                    split_policy,
-                ),
-                None => log(&tio, file, unbuffered, raw, depth),
-            };
-            result.map_err(|e| eprintln!("{:?}", e))
-        }
+                }) => meta_reroute(input, route, output),
+                None => log_metadata(&tio, file),
+            },
+            Some(LogSubcommands::Dump {
+                files,
+                data,
+                meta,
+                sensor,
+                depth,
+            }) => log_dump(files, data, meta, sensor, depth),
+            Some(LogSubcommands::Csv {
+                args,
+                sensor,
+                output,
+            }) => log_csv(args, sensor, output),
+            Some(LogSubcommands::Hdf {
+                files,
+                output,
+                filter,
+                compress,
+                debug,
+                split_level,
+                split_policy,
+            }) => log_hdf(
+                files,
+                output,
+                filter,
+                compress,
+                debug,
+                split_level,
+                split_policy,
+            ),
+            None => log(&tio, file, unbuffered, raw, depth),
+        },
         Commands::Upgrade {
             tio,
             firmware_path,
             yes,
-        } => firmware_upgrade(&tio, firmware_path, yes).map_err(|e| eprintln!("{:?}", e)),
+        } => firmware_upgrade(&tio, firmware_path, yes),
         Commands::Completions { shell } => {
             clap_complete::generate(shell, &mut TioCli::command(), "tio", &mut std::io::stdout());
             Ok(())
         }
-    };
-
-    if result.is_ok() {
-        ExitCode::SUCCESS
-    } else {
-        eprintln!("FAILED");
-        ExitCode::FAILURE
     }
 }
