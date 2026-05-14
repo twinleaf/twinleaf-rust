5c9a5a56df118df21069f0bb9026d3d873a25560
Steven Nguyen <nguyen@twinleaf.com>
2026-05-14 10:41:06 -0400

    feat(twinleaf): re-export DeviceRoute and core identifiers from device/data

--- twinleaf/src/data/mod.rs
+++ twinleaf/src/data/mod.rs
@@ -8,6 +8,8 @@ mod subscription;
 #[cfg(feature = "hdf5")]
 pub mod export;
 
+pub use crate::tio::proto::identifiers::{ColumnKey, StreamId, StreamKey};
+pub use crate::tio::proto::DeviceRoute;
 pub use buffer::{AlignedWindow, Buffer, ColumnBatch, ReadError, RunId};
 pub use filter::ColumnFilter;
 pub use parser::{DeviceDataParser, DeviceFullMetadata};
--- twinleaf/src/device/mod.rs
+++ twinleaf/src/device/mod.rs
@@ -4,6 +4,7 @@ mod rpc;
 mod tree;
 pub mod util;
 
+pub use crate::tio::proto::DeviceRoute;
 pub use device::{Device, DeviceEvent, DeviceItem};
 pub use rpc::{RpcClient, RpcDescriptor, RpcList, RpcRegistry, RpcValue, RpcValueType};
 pub use tree::{DeviceTree, TreeEvent, TreeItem};
